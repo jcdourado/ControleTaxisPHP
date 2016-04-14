672f77445537dc7f836fee8d76d22447c1f9ae31
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-04-14 09:15:47 -0300

    Commit resource sem namespace

--- DAO/ResourceManager.class.php
+++ DAO/ResourceManager.class.php
@@ -1,10 +1,26 @@
 <?php
-  namespace DAO;
-
   class ResourceManager{
-    private static $banco = 'localhost';
-    private static $user = 
-  }
+    private static $server = 'localhost';
+    private static $user = 'taxi';
+    private static $senha = 'taxitaxi';
+    private static $banco = 'taxis';
+    private $conexao;
+    private static $resource;
 
+    private function ResourceManager(){
+      $this->conexao = new PDO("mysql:host=".self::$server .";dbname=".self::$banco, self::$user, self::$senha);
+    }
+
+    public function getConnection(){
+      return $this->conexao;
+    }
+
+    public static function getResource(){
+      if(!isset(self::$resource)){
+        self::$resource = new ResourceManager;
+      }
+      return self::$resource;
+    }
+  }
 
  ?>
